a9a48859b8f5d8051e68b2491bc6e9f378621874
Michael Musgrove <mmusgrov@redhat.com>
2018-07-04 13:24:24 +0100

    microprofile-lra cleanup unused directories

--- fractions/microprofile/microprofile-lra/coordinator/src.ok/main/java/io/narayana/lra/coordinator/api/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package io.narayana.lra.coordinator.api;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-// mark the war as a JAX-RS archive
-@ApplicationPath("/")
-public class JaxRsActivator extends Application {
-}
